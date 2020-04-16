e1f8059ed0b21e7748bd697d67e1ee75f44b516b
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-04-16 22:05:58 +0000

    Fixing style

--- shell.h
+++ shell.h
@@ -18,26 +18,26 @@
 extern char **environ;
 
 
-/* Funciones */
+/*📖 I N T E R A C T I V E  A N D  N O I N T E R A C T I V E */
 int interactive(char **env);
-int non_interactive(int argc ,char **argv, char **env);
-
-bool execute_command(char **commands, char *line, char **env);
+int non_interactive(int argc, char **argv, char **env);
+/** M A I N 🟥 F U N C T I O N S */
+bool exec_routes(char **commands, char *line, char **env);
 char **get_routes(char **commands, char **env);
 char *get_path_id(char **env);
-bool concat_commands(char **commands, char *line, char **env);
+bool execute_command(char **commands, char *line, char **env);
 char **get_commands(char *line);
-
-
-void free_commands(char **commands);
-
+void Error_fork(char *line, char **commands);
+/** 🆓F R E E  M E M O R Y */
+void free_arrays(char **array);
+/* 🏁S I G N A L  H A N D L E R  */
 void sighandle(int n __attribute__((unused)));
 void ctrl_d(char *line);
-
+/* 🛠 B U I L T  I N S   F U C T I O N S */
 void exit_shell(char *line, char **cmd);
-void print_env();
+void print_env(void);
 
-
+/** 🅰️ S T R I N G S   F U C T I O N S 🅰️*/
 int _strlen(char *str);
 int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
